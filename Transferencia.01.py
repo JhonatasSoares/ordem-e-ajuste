3cc81fa3e4624c08ce0d2eca33e3d808d0571144
JhonatasSoares <soaresjhonatasp@gmail.com>
2025-12-16 11:01:58 -0300

    atualização de vv

--- Transferencia.01.py
+++ Transferencia.01.py
@@ -825,7 +825,7 @@ def toggle_modo_headless():
 
 if __name__ == "__main__":
     root = ttk.Window(themename="litera")
-    root.title("🤖 Ordem e Ajuste 2.5.1")
+    root.title("🤖 Ordem e Ajuste 2.5.2")
     root.geometry("900x800")
     
     btn_headless = None
